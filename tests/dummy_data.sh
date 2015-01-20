68329e129cf4df294070fb6bd2aad0f0f2857c56
trodevel <trodevel@gmail.com>
2015-01-21 00:26:03 +0100

    sync with svn

--- tests/dummy_data.sh
+++ tests/dummy_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#$Id: dummy_data.sh 652 2014-06-30 16:01:01Z serge $
+# $Revision: 1419 $ $Date:: 2015-01-20 #$ $Author: serge $
 
 for s in 1 2 3 4 5; do echo -n "hello=$s&bla=$s$s& blax=777" | nc localhost 1234; echo; done;
